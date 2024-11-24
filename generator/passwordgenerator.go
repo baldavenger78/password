7f39e452b8ef50d7129212c5e9332b59a7d4e136
baldavenger78 <141161173+baldavenger78@users.noreply.github.com>
2024-11-24 08:31:33 +0100

    Create passwordgenerator.go

---
+++ generator/passwordgenerator.go
@@ -0,0 +1,21 @@
+package password
+
+import "github.com/baldavenger78/password.go"
+
+func GeneratePlainTextPassword() (string, error) {
+	pass, err := password.Generate(10, 2, 0, false, false)
+	if err != nil {
+		return "", err
+	}
+
+	return pass, nil
+}
+
+func GenerateRandomString() (string, error) {
+	pass, err := password.Generate(16, 8, 0, true, true)
+	if err != nil {
+		return "", err
+	}
+
+	return pass, nil
+}
